275a4ae6d82b1f1ea9d680d9ba89deb33198384b
Domonique Baul <dbaul1920@gmail.com>
2016-09-14 11:25:47 -0400

    outlining with comments

--- src/labSix.java
+++ src/labSix.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
 
 public class labSix {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// User greeting
+		//User input
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer that's greater than 0 but less than 10: ");
+		String word =input.nextLine();
+		
+		// Translator machine major if statement
+		
 
-	}
+		//check for vowel
+		for(int i>0; i<word.length();i++){
+			//index 0	
+			boolean vowel=true;
+						}
+		Object vowel;
+		if(vowel.equals(true))
+				{
+					// Then
+					//Add "way" to the end of the user input
+					//Print final value
+
+				}else{
+					//collect all letters (that are not vowels) before the first vowel
+						//store value of collection
+						//delete letters from word
+						//store value of remaining letters
+					//add collection to the end of remaining user input
+					//add "ay" to the end
+					//Print final value
+			}
 
 }
