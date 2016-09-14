779ec4331df7a1778a2c1dcff213f78601d53073
Domonique Baul <dbaul1920@gmail.com>
2016-09-14 16:23:07 -0400

    if not a vowel

--- src/labSix.java
+++ src/labSix.java
@@ -11,26 +11,30 @@ public class labSix {
 		Scanner input = new Scanner(System.in);
 		String userInput =input.next();
 		userInput.toLowerCase();
-		
+		//we need this to start before 0 because 0 is the index number
+		int indexOfVowel=-1;
 	
-		//define characters of vowel
-	
-	
+		//determine if first letter is a vowel
 		for(int i=0;i<userInput.length(); i++){
+			//if letter is a vowel just add say
 			if(userInput.charAt(i)=='a'||userInput.charAt(i)=='e'||userInput.charAt(i)=='i'||userInput.charAt(i)=='o'||userInput.charAt(i)=='u'){
 				System.out.println(userInput +"say");
-			}
-		
-		}}
-		
-		 //
-//				{
-//					// Then
-//					//Add "way" to the end of the user input
-//					//Print final value
-//					x
-//				
-//			}
-
-//}
+			}else{
+			//if starts with consent		
+				//loop through user input
+				for(int x=0;x<=userInput.length(); x++){
+					if(userInput.charAt(i)=='a'||userInput.charAt(i)=='e'||userInput.charAt(i)=='i'||userInput.charAt(i)=='o'||userInput.charAt(i)=='u'){
+					indexOfVowel=x;	
+					break;
+					}
+				
+			
+					
+				}
+				
+			}			
+		}
+	}
 }
+
+
